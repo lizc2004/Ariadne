8c56bd8b7f595f656d426dc42a2440a893b1885a
Noemi Coppotelli <liz.c2004@gmail.com>
2026-07-30 21:52:11 +0200

    implementa entita Utente e Task con enum Priorita

--- src/main/java/noemicoppotelli/ariadne/entities/Task.java
+++ src/main/java/noemicoppotelli/ariadne/entities/Task.java
@@ -1,4 +1,39 @@
 package noemicoppotelli.ariadne.entities;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+import noemicoppotelli.ariadne.enums.Priorita;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titolo;
+
+    @Column(nullable = false)
+    private String materia;
+
+    @Column(nullable = false)
+    private LocalDate scadenza ;
+
+    @Column(nullable = false)
+    private boolean completato;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Priorita priorita;
+
+    @ManyToOne
+    @JoinColumn(name = "utente_id")
+    private Utente utente;
 }
