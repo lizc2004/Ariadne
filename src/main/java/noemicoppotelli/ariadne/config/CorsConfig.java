b2ba279910fc62c8d7f9db8f928e86ba96fec1ff
Noemi Coppotelli <liz.c2004@gmail.com>
2026-08-26 16:26:27 +0200

    ripristina configurazione CORS mancante

--- src/main/java/noemicoppotelli/ariadne/config/CorsConfig.java
+++ src/main/java/noemicoppotelli/ariadne/config/CorsConfig.java
@@ -5,12 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
