cdfac7b22f015176d770a523ebc1fdd6427f391a
Noemi Coppotelli <liz.c2004@gmail.com>
2026-08-29 14:49:54 +0200

    espande endpoint progressi con task in scadenza, carte da ripassare e storico sessioni

--- src/main/java/noemicoppotelli/ariadne/payloads/ProgressiResponse.java
+++ src/main/java/noemicoppotelli/ariadne/payloads/ProgressiResponse.java
@@ -1,11 +1,12 @@
 package noemicoppotelli.ariadne.payloads;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class ProgressiResponse {
-    private int taskTotali;
-    private int taskCompletate;
-    private int minutiStudioUltimi7Giorni;
+    private List<TaskResponse> taskInScadenza;
+    private int carteDaRipassare;
+    private List<SessioneResponse> sessioni;
 }
